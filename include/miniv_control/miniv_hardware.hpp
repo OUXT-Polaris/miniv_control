dd86b139376d0739f6a7c217265469e5eed39135
Masaya Kataoka <ms.kataoka@gmail.com>
2021-03-28 15:18:37 +0900

    copy logic from crane_plus_control
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- include/miniv_control/miniv_hardware.hpp
+++ include/miniv_control/miniv_hardware.hpp
@@ -15,4 +15,68 @@
 #ifndef MINIV_CONTROL__MINIV_HARDWARE_HPP_
 #define MINIV_CONTROL__MINIV_HARDWARE_HPP_
 
+#include <miniv_control/visibility_control.hpp>
+
+#include <hardware_interface/base_interface.hpp>
+#include <hardware_interface/handle.hpp>
+#include <hardware_interface/hardware_info.hpp>
+#include <hardware_interface/system_interface.hpp>
+#include <hardware_interface/types/hardware_interface_return_values.hpp>
+#include <hardware_interface/types/hardware_interface_status_values.hpp>
+#include <rclcpp/macros.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <vector>
+
+using hardware_interface::return_type;
+
+namespace miniv_control
+{
+class MiniVHardware : public
+  hardware_interface::BaseInterface<hardware_interface::SystemInterface>
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(MiniVHardware)
+
+  MINIV_CONTROL_PUBLIC
+  ~MiniVHardware();
+
+  MINIV_CONTROL_PUBLIC
+  return_type configure(const hardware_interface::HardwareInfo & info) override;
+
+  MINIV_CONTROL_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  MINIV_CONTROL_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  MINIV_CONTROL_PUBLIC
+  return_type start() override;
+
+  MINIV_CONTROL_PUBLIC
+  return_type stop() override;
+
+  MINIV_CONTROL_PUBLIC
+  return_type read() override;
+
+  MINIV_CONTROL_PUBLIC
+  return_type write() override;
+
+private:
+  bool communication_timeout();
+
+  // std::shared_ptr<CranePlusDriver> driver_;
+  double timeout_seconds_;
+
+  std::vector<double> hw_position_commands_;
+  std::vector<double> hw_position_states_;
+  std::vector<double> hw_velocity_states_;
+  std::vector<double> hw_load_states_;
+  std::vector<double> hw_voltage_states_;
+  std::vector<double> hw_temperature_states_;
+
+  rclcpp::Time prev_comm_timestamp_;
+};
+}  // namespace miniv_control
+
 #endif  // MINIV_CONTROL__MINIV_HARDWARE_HPP_
--- src/miniv_hardware.cpp
+++ src/miniv_hardware.cpp
@@ -13,3 +13,254 @@
 // limitations under the License.
 
 #include <miniv_control/miniv_hardware.hpp>
+
+#include <hardware_interface/types/hardware_interface_type_values.hpp>
+
+namespace miniv_control
+{
+MiniVHardware::~MiniVHardware()
+{
+  // driver_->torque_enable(false);
+  // driver_->close_port();
+}
+
+return_type MiniVHardware::configure(
+  const hardware_interface::HardwareInfo & info)
+{
+  if (configure_default(info) != return_type::OK) {
+    return return_type::ERROR;
+  }
+
+  // Get parameters from URDF
+  // Initialize member variables
+  std::string port_name = info_.hardware_parameters["port_name"];
+  int baudrate = std::stoi(info_.hardware_parameters["baudrate"]);
+  timeout_seconds_ = std::stod(info_.hardware_parameters["timeout_seconds"]);
+
+  std::vector<uint8_t> dxl_id_list;
+  for (auto joint : info_.joints) {
+    if (joint.parameters["dxl_id"] != "") {
+      dxl_id_list.push_back(std::stoi(joint.parameters["dxl_id"]));
+    } else {
+      RCLCPP_ERROR(
+        rclcpp::get_logger("MiniVHardware"),
+        "Joint '%s' does not have 'dxl_id' parameter.",
+        joint.name.c_str());
+      return return_type::ERROR;
+    }
+  }
+
+  hw_position_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_position_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_velocity_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_load_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_voltage_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_temperature_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  // Open a crane_plus_driver
+  // driver_ = std::make_shared<CranePlusDriver>(port_name, baudrate, dxl_id_list);
+  /*
+  if (!driver_->open_port()) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("MiniVHardware"), driver_->get_last_error_log());
+    return return_type::ERROR;
+  }
+  if (!driver_->torque_enable(false)) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("MiniVHardware"), driver_->get_last_error_log());
+    return return_type::ERROR;
+  }
+  */
+
+  // Verify that the interface required by MiniVHardware is set in the URDF.
+  for (const hardware_interface::ComponentInfo & joint : info_.joints) {
+    if (joint.command_interfaces.size() != 1) {
+      RCLCPP_ERROR(
+        rclcpp::get_logger("MiniVHardware"),
+        "Joint '%s' has %d command interfaces found. 1 expected.",
+        joint.name.c_str(), joint.command_interfaces.size());
+      return return_type::ERROR;
+    }
+
+    if (joint.command_interfaces[0].name != hardware_interface::HW_IF_POSITION) {
+      RCLCPP_ERROR(
+        rclcpp::get_logger("MiniVHardware"),
+        "Joint '%s' have %s command interfaces found. '%s' expected.",
+        joint.name.c_str(), joint.command_interfaces[0].name.c_str(),
+        hardware_interface::HW_IF_POSITION);
+      return return_type::ERROR;
+    }
+  }
+
+  status_ = hardware_interface::status::CONFIGURED;
+  return return_type::OK;
+}
+
+std::vector<hardware_interface::StateInterface>
+MiniVHardware::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++) {
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, hardware_interface::HW_IF_POSITION,
+        &hw_position_states_[i])
+    );
+
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, hardware_interface::HW_IF_VELOCITY,
+        &hw_velocity_states_[i])
+    );
+
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, "load",
+        &hw_load_states_[i])
+    );
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, "voltage",
+        &hw_voltage_states_[i])
+    );
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, "temperature",
+        &hw_temperature_states_[i])
+    );
+  }
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface>
+MiniVHardware::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++) {
+    command_interfaces.emplace_back(
+      hardware_interface::CommandInterface(
+        info_.joints[i].name, hardware_interface::HW_IF_POSITION,
+        &hw_position_commands_[i])
+    );
+  }
+
+  return command_interfaces;
+}
+
+return_type MiniVHardware::start()
+{
+  /*
+  if (!driver_->torque_enable(false)) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("MiniVHardware"),
+      driver_->get_last_error_log());
+    return return_type::ERROR;
+  }
+  */
+  // Set current timestamp to disable the communication timeout.
+  prev_comm_timestamp_ = rclcpp::Clock().now();
+
+  // Set current joint positions to hw_position_commands.
+  read();
+  for (uint i = 0; i < hw_position_commands_.size(); i++) {
+    hw_position_commands_[i] = hw_position_states_[i];
+  }
+
+  status_ = hardware_interface::status::STARTED;
+  return return_type::OK;
+}
+
+return_type MiniVHardware::stop()
+{
+  // driver_->torque_enable(false);
+  status_ = hardware_interface::status::STOPPED;
+  return return_type::OK;
+}
+
+return_type MiniVHardware::read()
+{
+  if (communication_timeout()) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("MiniVHardware"), "Communication timeout!");
+    return return_type::ERROR;
+  }
+
+  std::vector<double> joint_positions;
+  /*
+  if (!driver_->read_present_joint_positions(&joint_positions)) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("MiniVHardware"),
+      driver_->get_last_error_log());
+    return return_type::ERROR;
+  } else {
+    for (uint i = 0; i < hw_position_states_.size(); ++i) {
+      hw_position_states_[i] = joint_positions[i];
+    }
+  }
+  */
+
+  // Disable read joint speeds, loads, voltages and temperatures
+  // to avoid a decrease of the communication rate.
+
+  // std::vector<double> joint_speeds;
+  // if (driver_->read_present_joint_speeds(&joint_speeds)) {
+  //   for (uint i = 0; i < hw_velocity_states_.size(); ++i) {
+  //     hw_velocity_states_[i] = joint_speeds[i];
+  //   }
+  // }
+
+  // std::vector<double> joint_loads;
+  // if (driver_->read_present_joint_loads(&joint_loads)) {
+  //   for (uint i = 0; i < hw_load_states_.size(); ++i) {
+  //     hw_load_states_[i] = joint_loads[i];
+  //   }
+  // }
+
+  // std::vector<double> joint_voltages;
+  // if (driver_->read_present_joint_voltages(&joint_voltages)) {
+  //   for (uint i = 0; i < hw_voltage_states_.size(); ++i) {
+  //     hw_voltage_states_[i] = joint_voltages[i];
+  //   }
+  // }
+
+  // std::vector<double> joint_temperatures;
+  // if (driver_->read_present_joint_temperatures(&joint_temperatures)) {
+  //   for (uint i = 0; i < hw_temperature_states_.size(); ++i) {
+  //     hw_temperature_states_[i] = joint_temperatures[i];
+  //   }
+  // }
+
+  prev_comm_timestamp_ = rclcpp::Clock().now();
+  return return_type::OK;
+}
+
+return_type MiniVHardware::write()
+{
+  if (communication_timeout()) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("MiniVHardware"), "Communication timeout!");
+    return return_type::ERROR;
+  }
+  /*
+  if (!driver_->write_goal_joint_positions(hw_position_commands_)) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("MiniVHardware"),
+      driver_->get_last_error_log());
+    return return_type::ERROR;
+  }
+  */
+
+  prev_comm_timestamp_ = rclcpp::Clock().now();
+  return return_type::OK;
+}
+
+bool MiniVHardware::communication_timeout()
+{
+  if (rclcpp::Clock().now().seconds() - prev_comm_timestamp_.seconds() >= timeout_seconds_) {
+    return true;
+  } else {
+    return false;
+  }
+}
+}  // namespace miniv_control
