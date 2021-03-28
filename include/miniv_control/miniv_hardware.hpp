0e920284303cc10d6480b5060998ef47c157361e
Masaya Kataoka <ms.kataoka@gmail.com>
2021-03-28 15:05:58 +0900

    add visibility control
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- include/miniv_control/miniv_hardware.hpp
+++ include/miniv_control/miniv_hardware.hpp
@@ -11,3 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#ifndef MINIV_CONTROL__MINIV_HARDWARE_HPP_
+#define MINIV_CONTROL__MINIV_HARDWARE_HPP_
+
+#endif  // MINIV_CONTROL__MINIV_HARDWARE_HPP_
